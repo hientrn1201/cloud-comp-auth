575a06dd9ead25ae63ff5aa96371adb410034895
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2024-12-12 16:20:38 +0700

    update jwt token auth

--- config.py
+++ config.py
@@ -9,7 +9,4 @@ class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PW', 'password')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'auth')
-
-    # JWT configuration
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'awaeoirasdfoiauf123')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'auth_db')
