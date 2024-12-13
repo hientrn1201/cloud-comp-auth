f02c9bf3e2eb0e36bd3f3e10abfdda22d01b501e
Prisha Samdarshi <prx2sam@gmail.com>
2024-12-13 00:24:04 -0500

    frontend url as env variable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import datetime
 from flask import Flask, request, jsonify, session
 from flasgger import Swagger
 from flasgger.utils import swag_from
@@ -7,6 +8,8 @@ from config import Config
 from models import init_db, register_user, get_user_by_id, get_all_users, update_user_in_db, delete_user_from_db, get_user_by_email
 from werkzeug.exceptions import abort
 import jwt
+from google.auth.transport import requests
+from google.oauth2 import id_token
 from flask_cors import CORS
 import os
 from swagger_definitions import (
@@ -27,12 +30,11 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config.from_object(Config)
 
-CORS(app)
-app.config['Access-Control-Allow-Origin'] = '*'
-app.config["Access-Control-Allow-Headers"] = "Content-Type"
+
+CORS(app, resources={r"/*": {"origins": os.getenv("FRONTEND_URL")}})
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = os.getenv("JWT_SECRET_KEY")
 algorithm = os.getenv("ALGORITHM", "HS256")
 
 # Initialize the database
@@ -43,11 +45,31 @@ swagger = Swagger(app)
 
 # JWT Functions
 
+# Your Google Client ID here (replace with actual)
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 
-def generate_jwt(payload):
-    """Generate a JWT token."""
-    return jwt.encode(payload, app.secret_key)
+# Endpoint to verify the Google token and generate a JWT
+@app.route('/api/generate_jwt', methods=['POST'])
+def generate_jwt():
+    google_token = request.json.get("googleToken")
+    if not google_token:
+        return jsonify({"error": "No token provided"}), 400
 
+    try:
+        # Verify the token with Google's public keys
+        id_info = id_token.verify_oauth2_token(google_token, requests.Request(), GOOGLE_CLIENT_ID)
+        user_email = id_info.get("email")
+
+        # Create a JWT token for your app
+        payload = {
+            "email": user_email,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }
+        jwt_token = jwt.encode(payload, app.secret_key, algorithm)
+
+        return jsonify({"jwt": jwt_token}), 200
+    except ValueError as e:
+        return jsonify({"error": "Invalid token"}), 401
 
 def decode_jwt(jwt_token):
     """Decode a JWT token."""
@@ -186,4 +208,4 @@ def log_response_info(response):
 
 
 if __name__ == '__main__':
-    app.run(port=8003, host="0.0.0.0", debug=True)
+    app.run(port=5002, host="0.0.0.0", debug=True)
